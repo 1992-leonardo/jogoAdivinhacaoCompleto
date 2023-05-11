69456d4b3280d0264c33d39bb0734ff5b64fa153
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-05-11 08:52:09 -0300

    Dando as boas vindas para o usuario.

--- jogoAdicinhacaoCompleto.cpp
+++ jogoAdicinhacaoCompleto.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <limits>
 
 int main() {
     char resposta = '\0';
-    while (true) {
+    while (true) 
+    {
         std::cout << "Digite S para sim ou N para nao: ";
         std::cin.get(resposta);
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        if (resposta == '\0' || resposta == '\n') {
+        if (resposta == '\0' || resposta == '\n') 
+        {
             std::cout << "Voce nao nos informou sua resposta. Tente novamente." << std::endl;
         }
-        else if (resposta == 'S' || resposta == 's') {
+        else if (resposta == 'S' || resposta == 's') 
+        {
             std::cout << "Legal poder contar com sua participacao no nosso jogo. " << std::endl;
             std::cout << "Seja muito bem vindo. *---------------*" << std::endl;
             break;
         }
-        else if (resposta == 'N' || resposta == 'n') {
+        else if (resposta == 'N' || resposta == 'n') 
+        {
             std::cout << "Ah que pena, nos vemos em outra hora entao! " << std::endl;
             std::cout << "Bye bye. Saindoooooooooooooooooooooooo!" << std::endl;
             exit(0);
         }
     }
-        std::cout << "Seja bem vindo ao nosso jogo da adivinhacao" << std::endl;
+
+    std::string nome;
+    while (nome.empty())
+    {
+        std::cout << "Qual eh o seu nome? " << std::endl;
+        std::getline(std::cin, nome);
+        std::cout << "\n\n";
+
+        if (nome.empty())
+        {
+            std::cout << "Voce nao nos informou o seu nome, tente novamente: " << std::endl;
+        }
+        else 
+        {
+            std::cout << "Olah " << nome << ". Muito bom te ter por aqui! " << std::endl;
+            std::cout << "Seja bem vindo ao nosso jogo da adivinhacao" << std::endl;
+            std::cout << "Vamos comecar..... " << std::endl;
+        }
+    }    
 
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
-Apenas comecei o meu jogo, fiz uma validacao, se a pessoa nao digitar nada, se a pessoa quiser jogar, e se a pessoa nao quiser jogar. Amanha eu dou continuidade no meu jogo, adicionando nele as demais validacoes. 
+10-05-2023 -> Apenas comecei o meu jogo, fiz uma validacao, se a pessoa nao digitar nada, se a pessoa quiser jogar, e se a pessoa nao quiser jogar. Amanha eu dou continuidade no meu jogo, adicionando nele as demais validacoes. 
+
+11-05-2023 -> Dando continuidade no meu projeto. Adicionei em meu codigo uma linha que solicita o nome do jogador, caso ele nao responda dar uma mensagem que nada foi digitado, apos ele responder, printar o nome dele na tela, e na sequencia, dar as boas vindas. 
 
