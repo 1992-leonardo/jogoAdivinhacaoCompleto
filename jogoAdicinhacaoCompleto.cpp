c06b486cb0e115898d8acd158ec8d783f05ef17d
Leonardo Possan <leonardopossamdasilva@gmail.com>
2023-05-16 21:55:56 -0300

    Finalizando o meu jogo da adivinhacao.

--- jogoAdicinhacaoCompleto.cpp
+++ jogoAdicinhacaoCompleto.cpp
@@ -1,50 +1,86 @@
 #include <iostream>
-#include <limits>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-    char resposta = '\0';
-    while (true) 
-    {
-        std::cout << "Digite S para sim ou N para nao: ";
-        std::cin.get(resposta);
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        if (resposta == '\0' || resposta == '\n') 
-        {
-            std::cout << "Voce nao nos informou sua resposta. Tente novamente." << std::endl;
-        }
-        else if (resposta == 'S' || resposta == 's') 
-        {
-            std::cout << "Legal poder contar com sua participacao no nosso jogo. " << std::endl;
-            std::cout << "Seja muito bem vindo. *---------------*" << std::endl;
-            break;
-        }
-        else if (resposta == 'N' || resposta == 'n') 
-        {
-            std::cout << "Ah que pena, nos vemos em outra hora entao! " << std::endl;
-            std::cout << "Bye bye. Saindoooooooooooooooooooooooo!" << std::endl;
-            exit(0);
+
+std::string obterSelecaoAleatoria() {
+    std::string selecoes[] = {"Brasil", "Alemanha", "Argentina"};
+    int indiceAleatorio = sizeof(selecoes) / sizeof(selecoes[0]);
+    return selecoes[rand() % indiceAleatorio];
+}
+
+void jogoAdivinhacao() {
+    std::string resposta;
+    while (resposta.empty()) {
+        std::cout << "Quer jogar o jogo da adivinhacao? (Digite 'sim' para jogar e nao para sair.): ";
+        std::getline(std::cin, resposta);
+
+        if (resposta.empty()) {
+            std::cout << "Voce nao digitou nada." << std::endl;
         }
     }
 
-    std::string nome;
-    while (nome.empty())
-    {
-        std::cout << "Qual eh o seu nome? " << std::endl;
+    if (resposta == "sim" || resposta == "Sim") {
+        std::string nome;
+        std::cout << "Qual eh o seu nome: ";
         std::getline(std::cin, nome);
-        std::cout << "\n\n";
+        std::cout << "Olah: " << nome  <<  ", seja bem vindo ao jogo da adivinhacao do LP. "<< std::endl;
 
-        if (nome.empty())
-        {
-            std::cout << "Voce nao nos informou o seu nome, tente novamente: " << std::endl;
-        }
-        else 
-        {
-            std::cout << "Olah " << nome << ". Muito bom te ter por aqui! " << std::endl;
-            std::cout << "Seja bem vindo ao nosso jogo da adivinhacao" << std::endl;
-            std::cout << "Vamos comecar..... " << std::endl;
-        }
-    }    
+        int nivelDificuldade;
+        std::cout << "Escolha o nivel de dificuldade (1 - Facil, 2 - Medio, 3 - Dificil): ";
+        std::cin >> nivelDificuldade;
+        std::cin.ignore();
 
+        int chances = (nivelDificuldade == 1) ? 10 : (nivelDificuldade == 2) ? 7 : (nivelDificuldade == 3) ? 5 : 0;
+
+        std::cout << "Vamos jogar! Adivinhe o nome de uma selecao de futebol." << std::endl;
+        std::string selecao = obterSelecaoAleatoria();
+        int numLetras = selecao.size();
+
+        std::string palpite;
+        std::string respostaParcial(numLetras, '_');
+
+        while (chances > 0) {
+            std::cout << "Palavra parcial: " << respostaParcial << std::endl;
+            std::cout << "Chances restantes: " << chances << std::endl;
+
+            std::cout << "Qual e o seu palpite? Digite uma letra: ";
+            std::getline(std::cin, palpite);
+
+            if (palpite.empty()) {
+                std::cout << "Voce nao digitou nada." << std::endl;
+                continue;
+            }
+
+            bool letraEncontrada = false;
+            for (int i = 0; i < numLetras; ++i) {
+                if (selecao[i] == palpite[0]) {
+                    respostaParcial[i] = palpite[0];
+                    letraEncontrada = true;
+                }
+            }
+
+            if (respostaParcial == selecao) {
+                std::cout << "Parabens, voce acertou! O nome da selecao e " << selecao << "." << std::endl;
+                return;
+            }
+
+            if (!letraEncontrada) {
+                std::cout << "Tente novamente. A letra '" << palpite << "' nao esta no nome da selecao." << std::endl;
+                --chances;
+            }
+        }
+
+        std::cout << "Suas chances acabaram. O nome da selecao era: " << selecao << std::endl;
+    } else {
+        std::cout << "Ah que pena, nos vemos em uma outra hora entao. " << std::endl;
+        std::cout << "Bye, saindooooooooo!" << std::endl;
+    }
+}
+
+int main() {
+    jogoAdivinhacao();
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 10-05-2023 -> Apenas comecei o meu jogo, fiz uma validacao, se a pessoa nao digitar nada, se a pessoa quiser jogar, e se a pessoa nao quiser jogar. Amanha eu dou continuidade no meu jogo, adicionando nele as demais validacoes. 
 
-11-05-2023 -> Dando continuidade no meu projeto. Adicionei em meu codigo uma linha que solicita o nome do jogador, caso ele nao responda dar uma mensagem que nada foi digitado, apos ele responder, printar o nome dele na tela, e na sequencia, dar as boas vindas. 
+11-05-2023 -> Dando continuidade no meu projeto. Adicionei em meu codigo uma linha que solicita o nome do jogador, caso ele nao responda dar uma mensagem que nada foi digitado, apos ele responder, printar o nome dele na tela, e na sequencia, dar as boas vindas. Dando sequencia as boas vindas! 
+
+16-05-2023 -> Finalizando o meu jogo e subindo o meu projeto, porem depois preciso melhorar um pouco o meu codigo. mais ate o momento o jogo esta finalizado. 
+
 
